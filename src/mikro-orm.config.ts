78997c22ccdc709ca60d03685ec594e0e7e7c674
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2021-03-05 14:11:56 +0700

    Login and Register
    with error handling

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constant";
 import { Post } from "./entities/Post";
 import path from 'path'
+import { User } from "./entities/User";
 
 export default {
     migrations: {
@@ -10,7 +11,7 @@ export default {
     },
     host: "127.0.0.1",
     port: 5432,
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'reddit-clone',
     type: 'postgresql',
     debug: !__prod__,
